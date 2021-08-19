d35afba7b7a9bd1633467846b512bcabc3933237
Buildkite <marcos.morelli@involves.com>
2021-08-19 10:49:01 -0300

    fixing build before tests

--- ci_run_tests.sh
+++ ci_run_tests.sh
@@ -5,13 +5,13 @@
 set -uo pipefail
 
 # Run the main command we're most interested in
+docker-compose build --pull
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose run test
 
 # Capture the exit status
 TESTS_EXIT_STATUS=$?
 
 # Run additional commands
-docker-compose build --pull
 docker-compose down
 
 # Exit with the status of the original command
