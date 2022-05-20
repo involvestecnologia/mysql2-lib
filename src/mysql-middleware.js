1354587eb502483dae8328721e5660913a9d9eee
pedro.borba <pedro.borba@involves.com>
2022-05-20 12:07:03 -0300

    option to inform different connection port

--- src/mysql-middleware.js
+++ src/mysql-middleware.js
@@ -5,7 +5,7 @@ const MysqlDatabase = require('./mysql-database')
 class MysqlMiddleware {
   static async setMysqlIntegration (req, _res, next) {
     const connectionPool = await MysqlConnection.getConnectionPool(
-      process.env.MYSQL_URL, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD)
+      process.env.MYSQL_URL, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, process.env.MYSQL_PORT)
     const mysql = new MysqlDatabase(connectionPool)
     if (!req.integrations) req.integrations = {}
     req.integrations.mysql = mysql
